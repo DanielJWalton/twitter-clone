9e52bda2cb3ec89a0f6baf83f97d52fec8ae94b3
DanielJWalton <dwaltonjr9425@gmail.com>
2022-05-23 13:09:31 -0700

    dark mode

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -135,7 +135,7 @@ function Tweet({ tweet }: Props) {
       )}
 
       {comments?.length > 0 && (
-        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-600 p-5">
+        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll scrollbar-hide border-t border-gray-600 p-5">
           {comments.map((comment) => (
             <div key={comment._id} className="relative flex space-x-2">
               <hr className="absolute left-5 top-10 h-8 border-x border-gray-600" />
